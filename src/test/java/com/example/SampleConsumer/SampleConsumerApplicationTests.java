390e9629889053422200e3c5d5e8149795f80c31
Ragav <Ragavendran.s2@cognizant.com>
2019-08-30 16:13:00 -0400

    Coverage improvements

--- src/test/java/com/example/SampleConsumer/SampleConsumerApplicationTests.java
+++ src/test/java/com/example/SampleConsumer/SampleConsumerApplicationTests.java
@@ -1,11 +1,15 @@
 package com.example.SampleConsumer;
 
 import com.example.SampleConsumer.controller.NumberTypeCheckerController;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,9 +18,24 @@ public class SampleConsumerApplicationTests {
 
 	@Autowired
 	private NumberTypeCheckerController numberTypeCheckerController;
+
+	private static ConfigurableApplicationContext appContext;
+
+
+	@BeforeClass
+	public static void startBootApp(){
+		appContext = SpringApplication.run(SampleConsumerApplication.class, "");
+	}
+
+
 	@Test
 	public void contextLoads() throws Exception {
 		Assert.assertNotNull(numberTypeCheckerController);
+	}
+
+	@AfterClass
+	public static void shutdownBootApp(){
+		appContext.close();
 	}
 
 }
