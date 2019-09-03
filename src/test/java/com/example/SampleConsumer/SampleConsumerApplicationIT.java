c2d2fd7496eabd469dc769b707e3da1d2365531a
Ragav <Ragavendran.s2@cognizant.com>
2019-09-03 11:56:21 -0400

    updated test cases and included gradlew

--- src/test/java/com/example/SampleConsumer/SampleConsumerApplicationIT.java
+++ src/test/java/com/example/SampleConsumer/SampleConsumerApplicationIT.java
@@ -1,41 +1,26 @@
 package com.example.SampleConsumer;
 
 import com.example.SampleConsumer.controller.NumberTypeCheckerController;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SampleConsumerApplicationTests {
+public class SampleConsumerApplicationIT {
 
 	@Autowired
 	private NumberTypeCheckerController numberTypeCheckerController;
 
 	private static ConfigurableApplicationContext appContext;
 
-
-	@BeforeClass
-	public static void startBootApp(){
-		appContext = SpringApplication.run(SampleConsumerApplication.class, "");
-	}
-
-
 	@Test
-	public void contextLoads() throws Exception {
-		Assert.assertNotNull(numberTypeCheckerController);
+	public void main() {
+		SampleConsumerApplication.main(new String[] {});
 	}
 
-	@AfterClass
-	public static void shutdownBootApp(){
-		appContext.close();
-	}
 
 }
--- src/test/java/com/example/SampleConsumer/service/NumberTypeCheckServiceImplTest.java
+++ src/test/java/com/example/SampleConsumer/service/NumberTypeCheckServiceImplTest.java
@@ -6,9 +6,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(JUnit4.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class NumberTypeCheckServiceImplTest {
 
     private NumberTypeCheckService numberTypeCheckService;
