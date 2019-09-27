d8ad2a879554cfdc85229929b9d8e7ab5f4c8546
Ragav <Ragavendran.s2@cognizant.com>
2019-09-27 12:00:34 -0400

    fixed test case

--- src/test/java/com/example/numbertypecheck/service/NumberTypeCheckServiceImplTest.java
+++ src/test/java/com/example/numbertypecheck/service/NumberTypeCheckServiceImplTest.java
@@ -33,7 +33,7 @@ public class NumberTypeCheckServiceImplTest {
 
         String result;
         result = numberTypeCheckService.checkNumberType(2);
-        Assert.assertEquals(NumberType.ODD.getValue(),result);
+        Assert.assertEquals(NumberType.EVEN.getValue(),result);
     }
 
 }
